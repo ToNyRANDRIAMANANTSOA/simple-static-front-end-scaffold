6dd3a5ce0e22fc2437785879cdf42400fd2c344d
ToNyRANDRIAMANANTSOA <tonyrandriamanantsoa.to@gmail.com>
2022-05-23 22:37:26 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+A simple scaffold to quickly set up a static front-end web ui.
+
+Just a customized version of [static-website-template](https://github.com/wwebdev/static-website-template)
---
+++ src/assets/js/main.js
@@ -0,0 +1,19 @@
+console.log('Calling Python :...');
+// window.eel.setVars(5, 3, 2, 0);
+
+// async function run() {
+//   //   let result = await window.eel.test_python(2)(); // Must prefix call with 'await', otherwise it's the same syntax
+//   let result = await window.eel.go()();
+//   console.log(
+//     'Got this from Python: \n' + result + '\n\n ' + JSON.parse(result)
+//   );
+//   result = JSON.parse(result);
+//   console.log(
+//     '\nlength: ' +
+//       result[0].length +
+//       '\nresult[0][0][3][5]' +
+//       result[0][0][3][5]
+//   );
+// }
+
+// run();
